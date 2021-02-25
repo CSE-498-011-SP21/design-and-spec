630da63f2784b0144197efeb9c32f8812c941ac4
Cody D'Ambrosio <cjd218@lehigh.edu>
2021-02-24 19:02:28 -0500

    Add Doxygen docs
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

---
+++ docs/html/search/all_67.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['getbackupservers',['getBackupServers',['../classServer.html#a71a34c248da1cb74f3453f06223a606e',1,'Server']]],
+  ['gethash',['getHash',['../classServer.html#adecf34082977620ca31ca8eab317cf6d',1,'Server']]],
+  ['getprimarykeys',['getPrimaryKeys',['../classServer.html#ad303d839086eee11137e6fc4a4bb95ab',1,'Server']]]
+];
---
+++ docs/html/search/functions_6c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['log_5fput',['log_put',['../classServer.html#a17fd9b41cd477cd9aafd19ac7edc1183',1,'Server']]]
+];
